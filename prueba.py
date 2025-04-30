d4157a709cc95005cb9eb83a90298547dbc39631
josedavidlo07 <jolopezos@unal.edu.co>
2025-04-29 19:22:56 -0500

    Archvios de prueba de mi compañero

---
+++ prueba.py
@@ -0,0 +1,6 @@
+#hola, este archivo es una prueba
+
+print("Hola, mundo!")
+print("Este es un archivo de prueba para el curso de Python.")
+print("Espero que funcione correctamente.")
+print("¡Hasta luego!")
